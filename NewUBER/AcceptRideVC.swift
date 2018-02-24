f116665ace70b84bd8f2f43bdce2d870b028e0a6
Ahmed Farido <AhmedFarido@github.com>
2018-02-24 04:27:59 +0200

    x

--- NewUBER/AcceptRideVC.swift
+++ NewUBER/AcceptRideVC.swift
@@ -3,7 +3,7 @@ import MapKit
 import FirebaseDatabase
 
 class AcceptRideVC: UIViewController {
-
+    
     @IBOutlet weak var Accept: UIButton!
     @IBOutlet weak var Map: MKMapView!
     
@@ -15,7 +15,7 @@ class AcceptRideVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
         let region = MKCoordinateRegion(center: requestLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         Map.setRegion(region, animated: false)
@@ -35,14 +35,24 @@ class AcceptRideVC: UIViewController {
     @IBAction func AcceptBUt(_ sender: Any) {
         
         Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: requesEmail).observe(.childAdded, with: { (snapshot) in
-          snapshot.ref.updateChildValues(["driverLat":self.driverlocation.latitude, "driverLon":self.driverlocation.longitude
-            ])
+            snapshot.ref.updateChildValues(["driverLat":self.driverlocation.latitude, "driverLon":self.driverlocation.longitude
+                ])
             Database.database().reference().child("RideRequests").removeAllObservers()
         })
         
-        
-        
-        
-        
+        let requestCLLocation = CLLocation(latitude: requestLocation.latitude, longitude: requestLocation.longitude)
+        CLGeocoder().reverseGeocodeLocation(requestCLLocation) { (placemarks, error) in
+            if let placemarks = placemarks {
+                if placemarks.count > 0 {
+                    let placeMark = MKPlacemark(placemark: placemarks[0])
+                    let mapItem = MKMapItem(placemark: placeMark)
+                    mapItem.name = self.requesEmail
+                    let options = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+                    mapItem.openInMaps(launchOptions: options)
+                    
+                    
+                }
+            }
+        }
     }
 }
